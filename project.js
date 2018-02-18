8eb7ae5f8010f6a55027986bfe88f5ab1a1979c1
Edward Guo <eddiesguo@gmail.com>
2018-02-17 22:02:15 -0800

    messages working 100%

--- project.js
+++ project.js
@@ -1,122 +1,124 @@
+var messages = [];
+
 function completedProject(projectName, endDate, budget, stylist, associate, imageUrls, projectID){
-    this.projectName = projectName;
-    this.endDate = endDate;
-    this.budget = budget;
-    this.stylist = stylist;
-    this.associate = associate;
-    this.imageUrls = imageUrls;
-    this.projectID = projectID;
+  this.projectName = projectName;
+  this.endDate = endDate;
+  this.budget = budget;
+  this.stylist = stylist;
+  this.associate = associate;
+  this.imageUrls = imageUrls;
+  this.projectID = projectID;
 
-    this.convertImageUrlToHtml = function(){
-      var ret = "";
-      var numOfImg = this.imageUrls.length;
+  this.convertImageUrlToHtml = function(){
+    var ret = "";
+    var numOfImg = this.imageUrls.length;
 
-      for(i = 0; i < numOfImg; i++){
-        ret += "<img src=\"" +imageUrls[i]+ "\" class=\"item\" alt=\"image1\">"
-      }
-      return ret;
+    for(i = 0; i < numOfImg; i++){
+      ret += "<img src=\"" +imageUrls[i]+ "\" class=\"item\" alt=\"image1\">"
     }
+    return ret;
+  }
 
-    this.html = function(){
-      return "      <div class=\"active_project\">\n" +
-                      "        <p class=\"project_name\">"+this.projectName+"</p>" +
-                      "        <p class=\"start_date\">End Date: "+ this.endDate +"</p>\n" +
-                      "        <p class=\"budget\">Budget: $"+budget+"</p>\n" +
-                      "        <p class=\"image\">" +this.convertImageUrlToHtml() +
-                      "        <p class=\"stylist_associate\">Stylist: "+ this.stylist+"<br>Associate: "+ this.associate +"</p>" +
-                      "      </div>";
-    }
+  this.html = function(){
+    return "      <div class=\"active_project\">\n" +
+    "        <p class=\"project_name\">"+this.projectName+"</p>" +
+    "        <p class=\"start_date\">End Date: "+ this.endDate +"</p>\n" +
+    "        <p class=\"budget\">Budget: $"+budget+"</p>\n" +
+    "        <p class=\"image\">" +this.convertImageUrlToHtml() +
+    "        <p class=\"stylist_associate\">Stylist: "+ this.stylist+"<br>Associate: "+ this.associate +"</p>" +
+    "      </div>";
+  }
 }
 
 function inactiveProject(projectName, budget, projectID){
-    this.projectName = projectName;
-    this.budget = budget;
-    this.projectID = projectID;
+  this.projectName = projectName;
+  this.budget = budget;
+  this.projectID = projectID;
 
-    this.html = function(){
-      return "      <div class=\"active_project\">\n" +
-                      "        <p class=\"project_name\">"+this.projectName+"</p>" +
-                      "        <p class=\"budget\">Budget: $"+budget+"</p>\n" +
-                      "        <p class=\"find_stylist\">Find Your Personal Stylist</p>" +
-                      "      </div>";
-    }
+  this.html = function(){
+    return "      <div class=\"active_project\">\n" +
+    "        <p class=\"project_name\">"+this.projectName+"</p>" +
+    "        <p class=\"budget\">Budget: $"+budget+"</p>\n" +
+    "        <p class=\"find_stylist\">Find Your Personal Stylist</p>" +
+    "      </div>";
+  }
 }
 
 function activeProject(projectName, endDate, budget, stylist, associate, progressUrl, projectID){
-    this.projectName = projectName;
-    this.endDate = endDate;
-    this.budget = budget;
-    this.stylist = stylist;
-    this.associate = associate;
-    this.progressUrl = progressUrl;
-    this.projectID = projectID;
+  this.projectName = projectName;
+  this.endDate = endDate;
+  this.budget = budget;
+  this.stylist = stylist;
+  this.associate = associate;
+  this.progressUrl = progressUrl;
+  this.projectID = projectID;
 
-    this.html = function(){
-      return "      <div class=\"active_project\">\n" +
-                      "        <p class=\"project_name\">"+this.projectName+"</p>" +
-                      "        <p class=\"start_date\">End Date: "+ this.endDate +"</p>\n" +
-                      "        <p class=\"budget\">Budget: $"+budget+"</p>\n" +
-                      "        <p class=\"image\"><p class=\"image\"><img src="+ this.progressUrl +" alt=\"Progress Image\" class=\"progress\"></p>"+
-                      "        <p class=\"stylist_associate\">Stylist: "+ this.stylist+"<br>Associate: "+ this.associate +"</p>" +
-                      "      </div>";
-    }
+  this.html = function(){
+    return "      <div class=\"active_project\">\n" +
+    "        <p class=\"project_name\">"+this.projectName+"</p>" +
+    "        <p class=\"start_date\">End Date: "+ this.endDate +"</p>\n" +
+    "        <p class=\"budget\">Budget: $"+budget+"</p>\n" +
+    "        <p class=\"image\"><p class=\"image\"><img src="+ this.progressUrl +" alt=\"Progress Image\" class=\"progress\"></p>"+
+    "        <p class=\"stylist_associate\">Stylist: "+ this.stylist+"<br>Associate: "+ this.associate +"</p>" +
+    "      </div>";
+  }
 }
 
 function getActiveProjectHtml(p){
-    return "      <div class=\"active_project\">\n" +
-                    "        <p class=\"project_name\">"+p.projectName+"</p>" +
-                    "        <p class=\"start_date\">End Date: "+ p.endDate +"</p>\n" +
-                    "        <p class=\"budget\">Budget: $"+budget+"</p>\n" +
-                    "        <p class=\"image\"><p class=\"image\"><img src="+ p.progressUrl +" alt=\"Progress Image\" class=\"progress\"></p>"+
-                    "        <p class=\"stylist_associate\">Stylist: "+ p.stylist+"<br>Associate: "+ p.associate +"</p>" +
-                    "      </div>";
+  return "      <div class=\"active_project\">\n" +
+  "        <p class=\"project_name\">"+p.projectName+"</p>" +
+  "        <p class=\"start_date\">End Date: "+ p.endDate +"</p>\n" +
+  "        <p class=\"budget\">Budget: $"+budget+"</p>\n" +
+  "        <p class=\"image\"><p class=\"image\"><img src="+ p.progressUrl +" alt=\"Progress Image\" class=\"progress\"></p>"+
+  "        <p class=\"stylist_associate\">Stylist: "+ p.stylist+"<br>Associate: "+ p.associate +"</p>" +
+  "      </div>";
 }
 
 function getCompletedProjectHtml(p){
   return "      <div class=\"active_project\">\n" +
-                  "        <p class=\"project_name\">"+p.projectName+"</p>" +
-                  "        <p class=\"start_date\">End Date: "+ p.endDate +"</p>\n" +
-                  "        <p class=\"budget\">Budget: $"+p.budget+"</p>\n" +
-                  // "        <p class=\"image\">" +p.convertImageUrlToHtml() +
-                  "        <p class=\"stylist_associate\">Stylist: "+ p.stylist+"<br>Associate: "+ p.associate +"</p>" +
-                  "      </div>";
+  "        <p class=\"project_name\">"+p.projectName+"</p>" +
+  "        <p class=\"start_date\">End Date: "+ p.endDate +"</p>\n" +
+  "        <p class=\"budget\">Budget: $"+p.budget+"</p>\n" +
+  // "        <p class=\"image\">" +p.convertImageUrlToHtml() +
+  "        <p class=\"stylist_associate\">Stylist: "+ p.stylist+"<br>Associate: "+ p.associate +"</p>" +
+  "      </div>";
 }
 function getInactiveProjectHtml(p){
 
   return "      <div class=\"active_project\">\n" +
-                  "        <p class=\"project_name\">"+p.projectName+"</p>" +
-                  "        <p class=\"budget\">Budget: $"+p.budget+"</p>\n" +
-                  "        <p class=\"find_stylist\">Find Your Personal Stylist</p>" +
-                  "      </div>";
+  "        <p class=\"project_name\">"+p.projectName+"</p>" +
+  "        <p class=\"budget\">Budget: $"+p.budget+"</p>\n" +
+  "        <p class=\"find_stylist\">Find Your Personal Stylist</p>" +
+  "      </div>";
 
 }
 
 function getNavBar(){
 
   return "    <div class=\"profile\" >\n"+
-                  "<img src=\"photos/steve.jpg\" alt=\"My Man Steve\">"+
-               "</div>"+
-              "<p class=\"heading\">John Doe<p>"+
-              "<p style=\"text-align:center;\">Edit Profile</p>"+
+  "<img src=\"photos/steve.jpg\" alt=\"My Man Steve\">"+
+  "</div>"+
+  "<p class=\"heading\">John Doe<p>"+
+  "<p style=\"text-align:center;\">Edit Profile</p>"+
 
-              "<div class=\"projects\" style=\"text-align:center;\">"+
-              "<p class=\"heading\">Projects</h1>"+
-              "<ul style=\" display:inline-block; text-align:left;\">"+
-                "<li><a href=\"active_project.html\">Active</a></li>"+
-                "<li><a href=\"inactive_project.html\">Inactive</a></li>"+
-                "<li><a href=\"completed_project.html\">Completed</a></li>"+
-                "<li><a href=\"create_project.html\">Create New</a></li></ul>"+
-              "</div>"+
-              "<a href=\"#\"><p class=\"heading\"> Messages </p></a>"+
-              "<a href=\"explore.html\"><p class=\"heading\"> Explore </p></a>"+
-              "<div style=\"text-align:center; !important\">"+
-                  "<ul style=\"display:inline-block; text-align:left; !important\">"+
-                  "<li><a href=\"#\">Help</a></li>"+
-                  "<li><a href=\"#\">Support</a></li>"+
-                  "<li><a href=\"#\">Feedback</a></li>"+
-                  "<li><a href=\"#\">Contact Us</a></li>"+
-                "</ul>"+
-              "</div>";
+  "<div class=\"projects\" style=\"text-align:center;\">"+
+  "<p class=\"heading\">Projects</h1>"+
+  "<ul style=\" display:inline-block; text-align:left;\">"+
+  "<li><a href=\"active_project.html\">Active</a></li>"+
+  "<li><a href=\"inactive_project.html\">Inactive</a></li>"+
+  "<li><a href=\"completed_project.html\">Completed</a></li>"+
+  "<li><a href=\"create_project.html\">Create New</a></li></ul>"+
+  "</div>"+
+  "<a href=\"#\"><p class=\"heading\"> Messages </p></a>"+
+  "<a href=\"explore.html\"><p class=\"heading\"> Explore </p></a>"+
+  "<div style=\"text-align:center; !important\">"+
+  "<ul style=\"display:inline-block; text-align:left; !important\">"+
+  "<li><a href=\"#\">Help</a></li>"+
+  "<li><a href=\"#\">Support</a></li>"+
+  "<li><a href=\"#\">Feedback</a></li>"+
+  "<li><a href=\"#\">Contact Us</a></li>"+
+  "</ul>"+
+  "</div>";
 }
 
 function addNavBar(p){
@@ -154,7 +156,7 @@ function populateInactiveProjectHtml(){
   p = JSON.parse(data);
   if(data != null){
     for(i = 0; i < p.length; i++)
-      addInactiveProject(p[i]);
+    addInactiveProject(p[i]);
   }
 }
 
@@ -165,7 +167,7 @@ function populateCompletedProjectHtml(){
   p = JSON.parse(data);
   if(data != null){
     for(i = 0; i < p.length; i++)
-      addCompletedProject(p[i]);
+    addCompletedProject(p[i]);
   }
 }
 
@@ -174,18 +176,41 @@ function populateActiveProjectHtml(){
   p = JSON.parse(data);
   if(data != null){
     for(i = 0; i < p.length; i++)
-      addInactiveProject(p[i]);
+    addInactiveProject(p[i]);
   }
 }
 
-function sendMessage(){
+function sendMessage(page){
   var input = document.getElementById("inputBox").value;
+
+  var messages = [];
+  messages = JSON.parse(localStorage.getItem("msg"));
+  if(!messages)
+  var messages = [];
+
   if(input === "")  return;
-  var chatBox = "<div style=\"border-radius: 15px; border: 1px solid black;\"><p class=\"chatContent\">"+input+"</p></div>";
-
-
+  messages.push({id:page, msg:input});
+  localStorage.setItem('msg', JSON.stringify(messages));
+  var chatBox = "<div class=\"chatBox\"><p class=\"chatContent\">"+input+"</p></div>";
   document.getElementById("inputBox").value =  "";
   document.getElementById("content").innerHTML += chatBox;
+}
+
+function populateMsg(page){
+  var messages = [];
+  //localStorage.clear();
+  messages = JSON.parse(localStorage.getItem("msg"));
+  console.log(messages);
+  if (!messages)
+  return;
+  for(i=0; i<messages.length; i++){
+    var input = messages[i].msg;
+    if(messages[i].id === page)
+      var chatBox = "<div class=\"chatBox\"><p class=\"chatContent\">"+input+"</p></div>";
+    else
+      var chatBox = "<div class=\"chatBox1\"><p class=\"chatContent\">"+input+"</p></div>";
+    document.getElementById("content").innerHTML += chatBox;
+  }
 }
 
 function finishIt(button){
